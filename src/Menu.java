86d09d629afcab1e12ce40a0d6768e5f3bb96c02
MaVirgil <magnusvirgil@hotmail.com>
2025-03-19 11:24:27 +0100

    Moved main menu to its own class

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,16 @@
 public class Menu {
+    //scanner object
+    //userInput
+
+    public static void mainMenu() {
+        System.out.println("""
+                === Velkommen til Marios pizzaria! ===
+                1: Vis menu
+                2: Vis ordreliste
+                3: Luk
+                """);
+
+        //handle userInput with switch case here:
+
+    }
 }
