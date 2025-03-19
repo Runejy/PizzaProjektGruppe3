c4113e26736b0174277597b76957ba8d9a258e66
MaVirgil <magnusvirgil@hotmail.com>
2025-03-19 12:01:13 +0100

    barebones input class

--- src/userInput.java
+++ src/userInput.java
@@ -1,2 +1,46 @@
+import java.util.Scanner;
+
 public class userInput {
+
+    private static String userInput;
+
+    private static final Scanner sc = new Scanner(System.in);
+
+    //Method for getting userInput when user should not pick from a list
+    public static String getUserInput() {
+
+        try {
+            userInput = sc.nextLine();
+        } catch (Exception e) {
+            System.out.println("An error occured: " + e);
+        }
+
+        return userInput;
+    }
+
+    public static String getUserInput(int numOptions) {
+
+        int userInt;
+
+        while(true) {
+
+            //get user input as integer, making sure they enter a number
+            try {
+                userInt = sc.nextInt();
+            } catch (Exception e) {
+                System.out.println("Indtast venligst et gyldigt nummer");
+                sc.nextLine();
+                continue;
+            }
+
+            //check if the users number is within the available options
+            if (userInt > numOptions || userInt <= 0) {
+                System.out.println("Indtast venligst et nummer mellem 1 og " + numOptions);
+                continue;
+            }
+
+            //convert userInt to String and return it
+            return String.valueOf(userInt);
+        }
+    }
 }
