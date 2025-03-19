96341d1d7bb5b98ef47536ebc812aeebb321d3d5
MaVirgil <magnusvirgil@hotmail.com>
2025-03-19 11:37:06 +0100

    to do comments in Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -3,6 +3,7 @@ public class Menu {
     //userInput
 
     public static void mainMenu() {
+
         System.out.println("""
                 === Velkommen til Marios pizzaria! ===
                 1: Vis menu
@@ -10,13 +11,31 @@ public class Menu {
                 3: Luk
                 """);
 
-        //handle userInput with switch case here
+        //handle userInput
+
+        //switch statement that calls other menu methods
 
     }
 
     public static void addOrderMenu() {
+
         System.out.println("Hvilken pizza vil du have?");
 
         //handle userInput here
+
+        //ask when the pizza will be collected
+
+        //handle userInput and save it as a localTime type
+    }
+
+    public static void printOrderList() {
+
+        System.out.println("\n=== Ordreliste ===");
+
+        //if order list is empty, print that
+
+        //print all orders as unordered list, sorted by time (soonest first)
+
+        //options to add, complete or cancel an order
     }
 }
