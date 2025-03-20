69c7d7ce616a95d65c1ec44ed495a5b1ec9580c8
404BrainNotFound1000 <emilgurres@gmail.com>
2025-03-20 22:36:18 +0100

    Added content to the Order CLass

--- src/Order.java
+++ src/Order.java
@@ -1,8 +1,52 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Order {
 
+    int order;
+    double time;
+    String name;
+    int phoneNr;
+    double totalPrice;
+
+    Scanner add = new Scanner(System.in);
+    String stringInput = add.nextLine();
+
     private static ArrayList<Order> orders = new ArrayList<>();
+
+    public Order(int order, double time, String name, int phoneNr, double totalPrice) {
+        this.order =order;
+        this.name = name;
+        this.time = time;
+        this.phoneNr = phoneNr;
+        this.totalPrice = totalPrice;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+    //mangler Scanner til telefonNr og en scanner til navn (Tiden er der en indbygget funktion til)
+    // @Override
+    //   public String toString() {
+    //     String s = "Navn;" + name + "\n tiden er; " + time + "\n telefon nr; " + phoneNr;
+    //   return s;
+    // }
+
+    //public addOrder() {
+    //  System.out.println("Skriv venligst følgende oplysninger: order, time, name, phineNr and totalPrice");
+    //input =
+    //}
+
+    //public void addPizza () {
+    //  System.out.println("Vælg venligst den pizza du vil have");
+    //input = add.nextLine();
+    // input
+
+    //}
 
     //constructor method (remember to add order to orders ArrayList)
 
@@ -27,3 +71,17 @@ public class Order {
 
 
 }
+
+public void main() {
+}
+
+
+
+
+//INstance fields for hvad en ordre skal inhdholde
+//En ArrayList holder styr på alle ordre
+//Constructor opretter et object i ordre klasse
+//Setter og getter metode
+}
+
+        }
