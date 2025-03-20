796ef1800b3da612f1a22ccc8e0e5c2cfc2dadbb
MaVirgil <magnusvirgil@hotmail.com>
2025-03-20 14:33:13 +0100

    comments outlining basic structure of Order class

--- src/Order.java
+++ src/Order.java
@@ -4,4 +4,26 @@ public class Order {
 
     private static ArrayList<Order> orders = new ArrayList<>();
 
+    //constructor method (remember to add order to orders ArrayList)
+
+    //---non-static methods---
+
+    //getPizzas (return all ordered pizzas, maybe as an arraylist)
+
+    //getTotalPrice
+
+    //getCustomerFirstName
+
+    //getCustomerPhone
+
+    //getCollectionTime
+
+    //toString (return all pertinent information about order as string)
+
+    //---static methods---
+
+    //printOrderList
+
+
+
 }
