69d2711489026271bf6e2c4ee5ca578295bad325
MaVirgil <magnusvirgil@hotmail.com>
2025-03-21 10:30:47 +0100

    added add() and printMenu() methods to Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -3,4 +3,15 @@ import java.util.ArrayList;
 public class Menu {
 
     private static ArrayList<PizzaType> menu = new ArrayList<>();
+
+    public static void add(PizzaType pt) {
+        menu.add(pt);
+    }
+
+    public static void printMenu() {
+
+        for (PizzaType pt : menu) {
+            System.out.println(pt);
+        }
+    }
 }
