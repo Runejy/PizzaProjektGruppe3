af876c441dc5aa205f1e702f1809ad5f9a5d4f97
Dirir <yadi0001@stud.kea.dk>
2025-03-21 09:53:19 +0100

    Instasieret fieldsne "this" metoden er blevet brugt på alle fieldsne Brugt "get" metode for "Price" & "name" Lavet en "toString" metode og retuneret et tomt ""

--- src/Order.java
+++ src/Order.java
@@ -1,29 +1,67 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Order {
+    int order;
+    double time;
+    String name;
+    int phoneNr;
+    double totalPrice;
 
+
+    //private static ArrayList<Order> orders = new ArrayList<>();
     private static ArrayList<Order> orders = new ArrayList<>();
+
+    public Order(int order, double time, String name, int phoneNr, double totalPrice) {
+        this.order = order;
+        this.name = name;
+        this.time = time;
+        this.phoneNr = phoneNr;
+        this.totalPrice = totalPrice;
+    }
+
+    //getCollectionTime
+    public double getTime() {
+        return time;
+    }
+
+    //getTotalPrice
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    //getCustomerPhone
+    public int getPhoneNr() {
+        return phoneNr;
+    }
+
+    //getCustomerFirstName
+    public String getName() {
+        return name;
+    }
+
+    //getPizzas (return all ordered pizzas, maybe as an arraylist)
+    public int getOrder() {
+        return order;
+    }
+
+    //toString (return all pertinent information about order as string)
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    public void addPizza() {
+        System.out.println("Vælg venligst den pizza du vil have");
+
+    }
 
     //constructor method (remember to add order to orders ArrayList)
 
     //---non-static methods---
 
-    //getPizzas (return all ordered pizzas, maybe as an arraylist)
-
-    //getTotalPrice
-
-    //getCustomerFirstName
-
-    //getCustomerPhone
-
-    //getCollectionTime
-
-    //toString (return all pertinent information about order as string)
-
     //---static methods---
-
-    //printOrderList
-
 
 
 }
+
