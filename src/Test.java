6914e70b57659d00e79164ba3335484df54d858b
MaVirgil <magnusvirgil@hotmail.com>
2025-03-25 11:22:42 +0100

    test for Input.getTime() method

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,18 @@
 public class Test {
     public static void main(String[] args) {
 
+
+        Menu.initializeMenu();
+
+        Order order = new Order();
+        order.setCustomerFirstName("John");
+        order.addItem(14);
+        order.addItem(14);
+        order.addItem(13);
+        System.out.println("Hvornår skal ordren hentes?");
+        String userInput = Input.getTime();
+        order.setCollectionTime(userInput);
+
+        System.out.println(Order.getAllOrders());
     }
 }
